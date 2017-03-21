0b791f0f34d3ad09ea1ff590342cee0afd4a22d8
Czenko, Marcin <marcin.czenko@philips.com>
2017-03-21 14:54:42 +0100

    Added styling to the Todo item.

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -4,4 +4,33 @@ export class Todo {
     @bindable id;
     @bindable text;
     @bindable completed;
+
+    constructor() {
+      this.styles = styles;
+    }
+
+    toggleCompleted() {
+      console.log('toggleCompleted');
+
+      this.completed = !this.completed;
+
+      this.updateStyle();
+    }
+
+    updateStyle() {
+      if (this.completed) {
+        this.styles = Object.assign({}, styles, { todo: styles.todoCompleted });
+      } else {
+        this.styles = Object.assign({}, styles, { todo: styles.todoNotCompleted });
+      }
+    }
 }
+
+const styles = {
+  todoCompleted: {
+    'text-decoration': 'line-through'
+  },
+  todoNotCompleted: {
+    'text-decoration': 'none'
+  }
+};
