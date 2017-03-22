b7c3047ed966d9df7b964e2ad3e83a32d0a46d99
Czenko, Marcin <marcin.czenko@philips.com>
2017-03-22 13:30:10 +0100

    Added some styling and and improved user experience.

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -23,18 +23,35 @@ export class Todo {
 
     updateStyle() {
       if (this.completed) {
-        this.styles = Object.assign({}, styles, { todo: styles.todoCompleted });
+        this.styles = Object.assign({}, styles, { todo: {...styles.todoCompleted} });
       } else {
-        this.styles = Object.assign({}, styles, { todo: styles.todoNotCompleted });
+        this.styles = Object.assign({}, styles, { todo: {...styles.todoNotCompleted} });
       }
     }
 }
 
 const styles = {
+  container: {
+    display: 'flex',
+    'flex-flow': 'row nowrap',
+    'align-items': 'center',
+    width: '100%'
+  },
+  buttonContainer: {
+    flex: 1,
+    display: 'block',
+    margin: '0 10px'
+  },
+  button: {
+    padding: '15px'
+  },
   todoCompleted: {
+    flex: 1,
     'text-decoration': 'line-through'
   },
   todoNotCompleted: {
+    flex: 1,
     'text-decoration': 'none'
-  }
+  },
+
 };
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -3,14 +3,51 @@ import { bindable, observable } from 'aurelia-framework';
 export class TodoList {
   @bindable props;
 
-  todoText;
+  @observable todoText;
+
+  constructor() {
+    this.styles = styles;
+    this.buttonDisabled = 'disabled';
+  }
 
   propsChanged() {
     console.log('propsChanged=', this.props);
   }
 
+  todoTextChanged() {
+    console.log('this.todoText.trim().lenght', this.todoText.trim().length)
+    if (this.todoText.trim().length === 0) {
+      this.buttonDisabled = 'disabled';
+    } else {
+      this.buttonDisabled = '';
+    }
+  }
+
   addTodo() {
     this.props.todoAdd(this.props.todos.length, this.todoText);
     this.todoText = '';
+  }
+}
+
+const styles = {
+  container: {
+    margin: '10px',
+    padding: 0,
+    width: '600px'
+  },
+
+  todoList: {
+    display: 'flex',
+    'flex-flow': 'column nowrap'
+  },
+
+  inputGroup: {
+    display: 'flex',
+    margin: '10px 0'
+  },
+
+  button: {
+    'margin-left': '15px',
+    'padding': '10px'
   }
 }
